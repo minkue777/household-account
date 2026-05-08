25527bb3bb6791f99e0286e5217162a690305130
minkue777 <minkue777@gmail.com>
2026-05-08 18:17:14 +0900

    릴리즈 v1.2.5 준비

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.household.account"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.4"
+        versionCode = 7
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
