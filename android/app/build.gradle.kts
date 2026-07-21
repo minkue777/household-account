2a4085e5bb7051d121e64f83bb25b1bcc23592d3
minkue777 <minkue777@gmail.com>
2026-07-21 23:50:27 +0900

    릴리즈 v1.2.9 준비

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.household.account"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.8"
+        versionCode = 11
+        versionName = "1.2.9"
 
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$googleWebClientId\"")
 
